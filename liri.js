8dea4709c147d3f01c358d89cf2487a78e6a1edf
robertsonm30 <robertsonm30@gmail.com>
2018-02-26 18:32:08 -0500

    update

--- liri.js
+++ liri.js
@@ -1,8 +1,43 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var usera = process.argv[2];
+var userb = process.argv[3];
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
+
+if (usera === "my-tweets") {
+	client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+	console.log(tweets);
+	});
+} else if (userb === "spotify-this-song" ){
+	spotify.search({ type: 'track', query: userb }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+
+	console.log(data); 
+	});
+} else {
+	return;
+}
+
+
 
 // * `my-tweets`
+
+
+
 
 // * `spotify-this-song`
 
